39294701e8a83adb3e5a6174536c9fe707df45a1
Nicholas Carsner <nicholascarsner@gmail.com>
2026-09-17 04:31:37 -0500

    Persist figures: add save_plots and return figures from plot functions
    Every plotting function ended at plt.show(), so no figure could be
    written to disk.
    
    - save_plots provides save_figure (matplotlib), save_plotly_figure
      (HTML) and save_map (folium), each creating parent directories.
    - matplotlib_plots functions return their Figure and accept show=True.
      The default preserves existing interactive behavior.
    - set_plot_style() moves from matplotlib_plots to utils/plot_styles.
    
    Refs #2

---
+++ src/utils/plot_styles.py
@@ -0,0 +1,11 @@
+import seaborn as sns
+
+
+def set_plot_style(style="whitegrid"):
+    """
+    Set the style for the plots.
+
+    Parameters:
+    style (str): The style to use for the plots. Default is 'whitegrid'.
+    """
+    sns.set_style(style)
